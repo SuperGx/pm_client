7b0d192f6e7dd8cabeda50e898a403577434b1c8
SuperGx <mitreageorge14@stud.ase.ro>
2021-05-30 01:11:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pwdmanager/RegisterActivity.java
@@ -0,0 +1,218 @@
+package com.example.pwdmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSocketFactory;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button registerButton;
+    SSLSocketFactory sslSocketFactory;
+    final static String myDomainIp = "https://192.168.13.14";
+    final static String myDomainPort = "8443";
+    EditText etName;
+    EditText etEmail;
+    EditText etPwd;
+    EditText etPwd2;
+    String name;
+    String email;
+    String password;
+    String password2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+
+        textView = (TextView) findViewById(R.id.text);
+        registerButton = findViewById(R.id.btnRegister);
+        etName = findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
+        etPwd = findViewById(R.id.etPwd);
+        etPwd2 = findViewById(R.id.etPwd2);
+
+
+
+        try {
+            sslSocketFactory = TrustMyCert.getSocketFactory(getApplicationContext());
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = etName.getText().toString();
+                email = etEmail.getText().toString();
+                password = etPwd.getText().toString();
+                password2 = etPwd2.getText().toString();
+
+                if(validateInput()) {
+                    try {
+                        registerNewUser();
+                    } catch (InvalidKeySpecException e) {
+                        e.printStackTrace();
+                    } catch (NoSuchAlgorithmException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        })
+        ;
+    }
+
+    private boolean validateInput(){
+        boolean isAnyError = false;
+        if(name.isEmpty())
+        {
+            etName.setError("Name empty!");
+            isAnyError = true;
+        }
+
+        if (name.length() < 3)
+        {
+            etName.setError("Name must be at least 3 chars!");
+            isAnyError = true;
+        }
+
+        if(email.isEmpty())
+        {
+            etEmail.setError("Email empty!");
+            isAnyError = true;
+        }
+
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            etEmail.setError("Invalid email!");
+            isAnyError = true;
+        }
+
+        if(password.length() < 8)
+        {
+            etPwd.setError("Password must be at least 8 chars!");
+            isAnyError = true;
+        }
+
+        if(!password.equals(password2))
+        {
+            etPwd2.setError("Passwords do not match!");
+            isAnyError = true;
+        }
+
+        if(isAnyError)
+        {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+
+    private void registerNewUser() throws InvalidKeySpecException, NoSuchAlgorithmException {
+        // Set to trust my self signed certificate
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslSocketFactory);
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = myDomainIp + ":" + myDomainPort + "/register";
+        String hashedPassword = hashPassword();
+
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("firstName", name);
+        params.put("email", email);
+        params.put("password", hashedPassword);
+
+        JsonObjectRequest req = new JsonObjectRequest(url, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Toast.makeText(getApplicationContext(), "SUCCESS!", Toast.LENGTH_LONG).show();
+                            Log.d("pwddbg", response.toString(4));
+                            clearFields();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse networkResponse = error.networkResponse;
+                if (networkResponse != null && networkResponse.data != null) {
+                    String jsonError = new String(networkResponse.data);
+                    Log.e("pwderr", jsonError);
+                }
+            }
+        });
+        queue.add(req);
+
+    }
+
+//    private String hashPassword() throws NoSuchAlgorithmException, InvalidKeySpecException {
+//        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), "MySecret".getBytes(), 256, 256);
+//        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+//        byte[] derivative = skf.generateSecret(spec).getEncoded();
+//        spec.clearPassword();
+//        String hashedPassword = Base64.encodeToString(derivative, Base64.DEFAULT);
+//        return hashedPassword;
+//    }
+
+    private String hashPassword()
+    {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    private void clearFields()
+    {
+        etName.getText().clear();
+        etEmail.getText().clear();
+        etPwd.getText().clear();
+        etPwd2.getText().clear();
+        password = null;
+        password2 = null;
+    }
+
+}
---
+++ app/src/main/java/com/example/pwdmanager/TrustMyCert.java
@@ -0,0 +1,57 @@
+package com.example.pwdmanager;
+
+import android.content.Context;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+
+public class TrustMyCert {
+    public static SSLSocketFactory getSocketFactory(Context context)
+            throws CertificateException, IOException, KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+
+        // Load CAs from an InputStream (could be from a resource or ByteArrayInputStream or ...)
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+        InputStream caInput = new BufferedInputStream(context.getResources().openRawResource(R.raw.my));
+        // I paste my myFile.crt in raw folder under res.
+        Certificate ca;
+
+        //noinspection TryFinallyCanBeTryWithResources
+        try {
+            ca = cf.generateCertificate(caInput);
+            System.out.println("ca=" + ((X509Certificate) ca).getSubjectDN());
+        } finally {
+            caInput.close();
+        }
+
+        // Create a KeyStore containing our trusted CAs
+        String keyStoreType = KeyStore.getDefaultType();
+        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+        keyStore.load(null, null);
+        keyStore.setCertificateEntry("ca", ca);
+
+        // Create a TrustManager that trusts the CAs in our KeyStore
+        String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+        tmf.init(keyStore);
+
+        // Create an SSLContext that uses our TrustManager
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, tmf.getTrustManagers(), null);
+
+        return sslContext.getSocketFactory();
+    }
+}
